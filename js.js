e7e2f07c2ea47aebdcd6b014e9978638b9b85c65
AldanaAranda <aldanaaranda07@gmail.com>
2024-10-25 15:16:10 -0300

    agregando estilos

---
+++ js.js
@@ -0,0 +1,23 @@
+$('#carouselExample').carousel({
+    interval: 3000 // Cambia cada 3 segundos
+  });
+  
+  $('.carousel').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+  
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // añade los items al final
+        if (e.direction == "left") {
+          $('.carousel-item').eq(i).appendTo('.carousel-inner');
+        } else {
+          $('.carousel-item').eq(0).appendTo('.carousel-inner');
+        }
+      }
+    }
+  });
+  
